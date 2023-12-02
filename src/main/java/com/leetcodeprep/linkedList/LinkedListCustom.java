bf55384f90728e6a5863d3ec76a60665e413cb50
ImranArif536 <imran.arif1@outlook.com>
2023-12-02 12:42:14 +0500

    initial commit

---
+++ src/main/java/com/leetcodeprep/linkedList/LinkedListCustom.java
@@ -0,0 +1,30 @@
+package com.leetcodeprep.linkedList;
+
+public class LinkedListCustom <V>{
+    private LinkedListNode head;
+    private LinkedListNode tail;
+    private int nodeCount;
+
+    public LinkedListNode add(V val){
+        if(head == null){
+            head = new LinkedListNode();
+            head.value = val;
+            tail = head;
+        }
+        else {
+            this.tail.next = new LinkedListNode();
+            this.tail.next.value = val;
+            this.tail = this.tail.next;
+        }
+        nodeCount++;
+        return head;
+    }
+
+    public LinkedListNode get(int index){
+        LinkedListNode temp = head;
+        while(index -- > 0){
+            temp = temp.next;
+        }
+        return temp;
+    }
+}
